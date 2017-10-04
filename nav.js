34014763b1e82b69d8e530f39a0999493f016d67
Sanchit Sharma <sanchitsharma1@gmail.com>
2017-10-04 01:33:06 +0100

    Everything but menu toggle working

--- nav.js
+++ nav.js
@@ -1,9 +1,30 @@
+var menu = $('#menu');
+
+var toggleMenu = function () {
+  console.log("toggling menu");
+  // set timeout so that the panel has a chance to roll up
+  // before the menu switches states
+  if (menu.hasClass('open')) {
+      setTimeout(toggleHorizontal, 500);
+  }
+  else {
+      toggleHorizontal();
+  }
+  menu.toggleClass('open');
+  $('#toggle').toggleClass('x');
+};
+
+var toggleHorizontal = function () {
+  menu.toggleClass('open');
+};
+
 var initNavBar = function() {
 	var anchor = $("#page");
 	console.log(anchor.html());
 	$("#" + anchor.html()).addClass( "current" );
 	// In future the following line will replace the identifier with an img tag, so that we have a custom banner on each page.
 	anchor.html("");
+	$('toggle').click(toggleMenu);
 };
 
 var setup = function() {
