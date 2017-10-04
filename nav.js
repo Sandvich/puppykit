bac6b6782c05243f63f263a0ea6330e335253eaa
Sanchit Sharma <sanchitsharma1@gmail.com>
2017-10-04 12:11:29 +0100

    Fixed menu toggle and corrected the title on the empyrean page.

--- nav.js
+++ nav.js
@@ -1,21 +1,6 @@
-var menu = $('#menu');
-
 var toggleMenu = function () {
-  console.log("toggling menu");
-  // set timeout so that the panel has a chance to roll up
-  // before the menu switches states
-  if (menu.hasClass('open')) {
-      setTimeout(toggleHorizontal, 500);
-  }
-  else {
-      toggleHorizontal();
-  }
-  menu.toggleClass('open');
-  $('#toggle').toggleClass('x');
-};
-
-var toggleHorizontal = function () {
-  menu.toggleClass('open');
+    $('#menu').toggleClass('open');
+    $('#toggle').toggleClass('x');
 };
 
 var initNavBar = function() {
@@ -24,7 +9,7 @@ var initNavBar = function() {
 	$("#" + anchor.html()).addClass( "current" );
 	// In future the following line will replace the identifier with an img tag, so that we have a custom banner on each page.
 	anchor.html("");
-	$('toggle').click(toggleMenu);
+	$('#toggle').click(toggleMenu);
 };
 
 var setup = function() {
