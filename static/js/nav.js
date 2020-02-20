0def5ea330aa3a267c48d9ac3e97fa1fb126f4b7
Sanchit Sharma <sanchitsharma1@gmail.com>
2020-02-20 21:47:41 +0000

    Finished porting other than docker

--- static/js/nav.js
+++ static/js/nav.js
@@ -3,13 +3,4 @@ var toggleMenu = function () {
     $('#toggle').toggleClass('x');
 };
 
-var initNavBar = function() {
-	var anchor = $("#page");
-	console.log(anchor.html());
-	$("#" + anchor.html()).addClass( "current" );
-	// In future the following line will replace the identifier with an img tag, so that we have a custom banner on each page.
-	anchor.html("");
-	$('#toggle').click(toggleMenu);
-};
-
-$( document ).ready(initNavBar);
+$( document ).ready($('#toggle').click(toggleMenu));
