10e99a60959e14c34a99cefe247c5ce22c858f2a
Sanchit Sharma <sanchitsharma1@gmail.com>
2020-02-20 21:17:01 +0000

    Continuing the process of changing to templates.

--- main.py
+++ main.py
@@ -4,19 +4,50 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
+    # Renders the root page. Nothing special.
     return render_template('root.html', title="Main", highlight="index")
 
 @app.route("/<string:path>/")
 def main_paths(path):
+    # Renders all the pages directly under the root.
     if path in ['contact', 'personal', 'professional', 'projects', 'writing']:
         return render_template("%s.html" % path, title=path.capitalize(), highlight=path)
     else:
         abort(404)
 
-'''
-    Let's spend some time thinking about structure.
-    This mainfile should do just the core. What does that entail?
-'''
+@app.route("/personal/<string:path>/")
+def personal_paths(path):
+    # Everything under personal
+    if path in ['rpgs']:
+        return render_template("personal/%s.html" % path, title="Personal - %s" % path.capitalize(), highlight='personal')
+    else:
+        abort(404)
+
+@app.route("/projects/<string:path>/")
+def project_paths(path):
+    # Everything under projects
+    if path in ['accalia', 'elcm', 'empyrean', 'packmates']:
+        return render_template("projects/%s.html" % path, title="Projects - %s" % path.capitalize(), highlight='projects')
+    elif path == "marked_for_death":
+        return render_template("projects/marked_for_death.html", title="Projects - Marked for Death", highlight='projects')
+    else:
+        abort(404)
+
+@app.route("/writing/<string:path>/")
+def writing_paths(path):
+    # Everything under writing
+    valid = {
+        'accalia': 'The Accalia',
+        'finalstraw': 'The Final Straw',
+        'landthought': 'A Land of Thought',
+        'tippingpoint': 'Tipping Point',
+        'uprightdevotion': 'Upright Devotion',
+        'birth': 'Birth'
+    }
+    if path in valid.keys():
+        return render_template("writing/%s.html" % path, title="Writing - %s" % valid[path], highlight='writing')
+    else:
+        abort(404)
 
 if __name__ == "__main__":
     app.run()
