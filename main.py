45e345b847f6e67c337cae35b5245e8d59277689
Sanchit Sharma <sanchitsharma1@gmail.com>
2020-12-27 11:55:39 +0000

    Error pages!

--- main.py
+++ main.py
@@ -82,5 +82,14 @@ def writing_paths(path):
     else:
         abort(404)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error/404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error/500.html'), 500
+
 if __name__ == "__main__":
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
     app.run(debug=True)
